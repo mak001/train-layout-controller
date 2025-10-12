4513854941c2128de2686a2d64c95282ba4adb64
mak001 <mak001001@gmail.com>
2025-10-11 23:32:19 -0500

    added turnout and traverser/turntable commands

---
+++ src/io/serial/commands/TraverserCommands.js
@@ -0,0 +1,118 @@
+import Command from './Command';
+
+/** Command to list all traversers/turntables
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#i-request-a-list-all-defined-turntables-traversers
+ */
+class ListTraversersCommand extends Command {
+  constructor() {
+    super('I');
+  }
+}
+
+/**
+ * Command to request the position of a traverser/turntable
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#i-id-request-position-of-the-specified-turntable-traverser
+ */
+class TraverserPositionCommand extends Command {
+  /**
+   * @param {number|string} id The id of the traverser/turntable
+   */
+  constructor(id) {
+    super('I', id);
+  }
+}
+
+/**
+ * Command to rotate a turntable to a specific position
+ */
+class RotateTurntableCommand extends Command {
+  /**
+   * @param {number|string} id The id of the traverser/turntable
+   * @param {number|string} position The position to rotate to
+   * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#i-id-position-rotate-a-dcc-turntable
+   */
+  constructor(id, position) {
+    super('I', id, position);
+  }
+}
+
+/**
+ * Command to rotate an Ex turntable to a specific position and perform an activity
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#i-id-position-activity-rotate-ex-turntable
+ * @see https://dcc-ex.com/ex-turntable/test-and-tune.html#ex-turntable-activity-reference
+ */
+class RotateExTurntableCommand extends Command {
+  /**
+   * Turntable activities
+   * @enum {number}
+   * @readonly
+   * @static
+   */
+  static ACTIVITIES = Object.freeze({
+    TURN: 0,
+    TURN_PINVERT: 1,
+    HOME: 2,
+    CALIBRATE: 3,
+    LED_ON: 4,
+    LED_SLOW: 5,
+    LED_FAST: 6,
+    LED_OFF: 7,
+    ACC_ON: 8,
+    ACC_OFF: 9,
+  });
+
+  /**
+   * @param {strin|number} id The id of the turntable
+   * @param {string|number} position The position to rotate to
+   * @param {RotateExTurntableCommand.ACTIVITIES} activity The activity to perform
+   */
+  constructor(id, position, activity) {
+    super('I', id, position, activity);
+  }
+}
+
+/**
+ * Command to get the list of all traversers/turntables
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#j-o-jo-request-the-list-of-defined-turntables-traversers
+ */
+class GetTraverserListCommand extends Command {
+  constructor() {
+    super('JO');
+  }
+}
+
+/**
+ * Command to request the status of a traverser/turntable
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#j-o-id-jo-id-request-details-of-the-specific-turntable-traverser
+ */
+class TraverserStatusCommand extends Command {
+  /**
+   * @param {number|string} id The id of the traverser/turntable
+   */
+  constructor(id) {
+    super('JO', id);
+  }
+}
+
+/**
+ * Command to request the position of a traverser/turntable
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#j-p-id-jp-id-request-all-position-details-of-the-specified-turntable-traverser
+ */
+class TraverserAllPositionCommand extends Command {
+  /**
+   * @param {number|string} id The id of the traverser/turntable
+   */
+  constructor(id) {
+    super('JP', id);
+  }
+}
+
+export default {
+  ListTraversersCommand,
+  TraverserPositionCommand,
+  RotateTurntableCommand,
+  RotateExTurntableCommand,
+  GetTraverserListCommand,
+  TraverserStatusCommand,
+  TraverserAllPositionCommand,
+};
---
+++ src/io/serial/commands/TurnoutCommands.js
@@ -0,0 +1,66 @@
+import Command from './Command';
+
+/**
+ * Command to request the list of all turnouts and states
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#t-request-a-list-all-defined-turnouts-points
+ */
+class RequestTurnoutListCommand extends Command {
+  constructor() {
+    super('T');
+  }
+}
+
+/** Command to throw or close a turnout
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#t-id-state-throw-or-close-a-defined-turnout-point
+ */
+class ThrowTurnoutCommand extends Command {
+  /**
+   * Turnout states
+   * @enum {string}
+   * @readonly
+   * @static
+   */
+  static STATES = Object.freeze({
+    THROWN: 'T',
+    CLOSED: 'C',
+    EXAMINE: 'X',
+  });
+
+  /**
+   * @param {number|string} id The id of the turnout
+   * @param {ThrowTurnoutCommand.STATES} state The state to set the turnout to
+   */
+  constructor(id, state) {
+    super('T', id, state);
+  }
+}
+
+/**
+ * Command to request the status of a turnout
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#j-t-id-jt-id-request-details-of-a-specific-turnout-point
+ */
+class TurnoutStatusCommand extends Command {
+  /**
+   * @param {number|string} id The id of the turnout
+   */
+  constructor(id) {
+    super('JT', id);
+  }
+}
+
+/**
+ * Command to list all turnout ids
+ * @see https://dcc-ex.com/reference/software/command-summary-consolidated.html#j-t-jt-request-the-list-of-defined-turnout-point-ids
+ */
+class ListTurnoutsCommand extends Command {
+  constructor() {
+    super('JT');
+  }
+}
+
+export default {
+  RequestTurnoutListCommand,
+  ThrowTurnoutCommand,
+  TurnoutStatusCommand,
+  ListTurnoutsCommand,
+};
