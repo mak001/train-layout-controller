e08653deeba5ebd9d14d0caa85079e3a32a8f252
mak001 <mak001001@gmail.com>
2025-10-17 22:03:53 -0500

    added basic js test file for the serial controller

---
+++ src/controller/io/IOControllerTets.js
@@ -0,0 +1,10 @@
+import IOController from './IOController';
+
+const controller = new IOController({
+  serial: {
+    port: process.env.SERIAL_PORT || '/dev/ttyUSB0',
+    baudRate: 115200,
+  },
+  commandHistorySize: 50,
+  responseHistorySize: 50,
+});
